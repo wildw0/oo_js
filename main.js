f2b146d4006f176e22e80bbf623828b3174d7e50
wildw0 <126777461+wildw0@users.noreply.github.com>
2023-11-08 15:55:35 +0300

    modified main.js & update branch name

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-function Atleta (nome, modalidade, medalhas, patrocinio, valor_Recebido) {
+function Artista (nome, modalidade, apresentacaoes, valor_Recebido) {
     this.nome = nome;
     this.modalidade = modalidade;
-    this.medalhas = medalhas;
-    this.patrocinio = patrocinio;
+    this.apresentacaoes = apresentacaoes;
     this.valor_Recebido = valor_Recebido;
 }
 
-const atleta1 = new Atleta ("Carlos", "Boxe", 4, "sim", 7000);
-const atleta2 = new Atleta ("Paula", "Tênis", 2, "não", null);
+function Atleta (nome, modalidade, medalhas, valor_Recebido) {
+    this.nome = nome;
+    this.modalidade = modalidade;
+    this.medalhas = medalhas;
+    this.valor_Recebido = valor_Recebido;
+}
+
+const atleta1 = new Atleta ("Carlos", "Boxe", 4, 7000);
+const atleta2 = new Atleta ("Paula", "Tênis", 2, null);
 
 console.log (atleta1);
 console.log (atleta2);
