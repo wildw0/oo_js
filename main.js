d90f092f0d768cb194bd992d46907c96432eba30
wildw0 <126777461+wildw0@users.noreply.github.com>
2023-11-10 15:25:39 +0300

    Correcao no codigo para entrega

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-function Artista (nome, modalidade, apresentacaoes, valor_Recebido) {
-    this.nome = nome;
-    this.modalidade = modalidade;
-    this.apresentacaoes = apresentacaoes;
-    this.valor_Recebido = valor_Recebido;
-}
+function Patrocinado(nome, modalidade, valor_Recebido) {
+        this.nome = nome;
+        this.modalidade = modalidade;
+        this.valor_Recebido = valor_Recebido;
+    }
 
-function Atleta (nome, modalidade, medalhas, valor_Recebido) {
-    this.nome = nome;
-    this.modalidade = modalidade;
-    this.medalhas = medalhas;
-    this.valor_Recebido = valor_Recebido;
-}
+function Artista (nome, modalidade, apresentacaoes, valor_Recebido,) {
+    Patrocinado.call(this, nome, modalidade, valor_Recebido)
+        this.apresentacaoes = apresentacaoes;
+    }
 
-const atleta1 = new Atleta ("Carlos", "Boxe", 4, 7000);
-const atleta2 = new Atleta ("Paula", "Tênis", 2, null);
+const artista1 = new Artista("Kurt", "Cantor", 124, 10000);
+const artista2 = new Artista("Tarsila", "Pintora", null, 12500);
+
+console.log (artista1);
+console.log (artista2);
+
+
+function Atleta (nome, modalidade, medalhas, valor_Recebido,) {
+    Patrocinado.call(this, nome, modalidade, valor_Recebido)
+        this.medalhas = medalhas;
+    }
+
+const atleta1 = new Atleta("Maguila", "Boxe", 4, 1000);
+const atleta2 = new Atleta("Carmen", "Atletismo", 26, 12800);
 
 console.log (atleta1);
 console.log (atleta2);
